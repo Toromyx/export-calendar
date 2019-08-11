dc72ec29c748c389ae849978ee1bbb31e57259fa
Thomas Gauges <thomas.gauges@gmail.com>
2019-08-11 19:17:41 +0200

    feat: make project npm package, install prettier to format lua (+ editorconfig for xml)

--- src/controllers/slash.lua
+++ src/controllers/slash.lua
@@ -1,35 +1,33 @@
-SLASH_CALENDAREXPORT1 = "/calexp"
+SLASH_CALENDAREXPORT1 = '/calexp'
 
 function printHelp()
-    local events = CalendarExport.calendar.getAllEvents()
-    print("Type '/calexp x' with being x one of:")
-    for i, event in ipairs(events) do
-        print(i .. ": " .. event.title .. "(" .. event.startTime.hour .. ":" ..
-                  event.startTime.minute .. ")")
-    end
+  local events = CalendarExport.calendar.getAllEvents()
+  print("Type '/calexp x' with being x one of:")
+  for i, event in ipairs(events) do
+    print(i .. ': ' .. event.title .. '(' .. event.startTime.hour .. ':' .. event.startTime.minute .. ')')
+  end
 end
 
 function SlashCmdList.CALENDAREXPORT(msg)
-    local events = CalendarExport.calendar.getAllEvents()
-    if (#events == 0) then
-        print("There are no events today")
-        return
-    end
-    if (msg == "") then
-        printHelp()
+  local events = CalendarExport.calendar.getAllEvents()
+  if (#events == 0) then
+    print('There are no events today')
+    return
+  end
+  if (msg == '') then
+    printHelp()
+  else
+    local index = tonumber(msg)
+    if (index and CalendarExport.calendar.getEvent(index)) then
+      local event = CalendarExport.calendar.getEvent(index)
+      local inviteList = CalendarExport.calendar.getInviteList(event.eventID)
+      local csv = CalendarExport.csv.formatCsv(inviteList)
+      CalendarExport.frames.export.fillContent(csv)
+      CalendarExport.frames.export.show()
+      CalendarExport.frames.export.selectAll()
     else
-        local index = tonumber(msg)
-        if (index and CalendarExport.calendar.getEvent(index)) then
-            local event = CalendarExport.calendar.getEvent(index)
-            local inviteList = CalendarExport.calendar.getInviteList(
-                                   event.eventID)
-            local csv = CalendarExport.csv.formatCsv(inviteList)
-            CalendarExport.frames.export.fillContent(csv)
-            CalendarExport.frames.export.show()
-            CalendarExport.frames.export.selectAll()
-        else
-            print("No valid event index given")
-            printHelp()
-        end
+      print('No valid event index given')
+      printHelp()
     end
+  end
 end
--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,3 @@
 CalendarExport = {
-    frames = {}
+  frames = {}
 }
