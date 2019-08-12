0cce85b85222c47cc96f2c1195cf981993506b98
Thomas Gauges <thomas.gauges@gmail.com>
2019-08-12 19:24:52 +0200

    feat: use blizzard debug dump function when ViragDevTool is not installed

--- src/services/debug.lua
+++ src/services/debug.lua
@@ -2,6 +2,8 @@ function CalendarExport.debug(data, name)
   name = name or 'CalendarExport Debug Data'
   if ViragDevTool_AddData then
     ViragDevTool_AddData(data, name)
+  elseif DevTools_Dump then
+    DevTools_Dump(data)
   else
     print(data)
   end
