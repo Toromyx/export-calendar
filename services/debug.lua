040332ac4160478de23a796c00d18e8f2c4b92b0
Thomas Gauges <thomas.gauges@gmail.com>
2019-08-17 19:23:56 +0200

    reformat: add debug comments (curseforge automatic packaging) and reformat file

--- services/debug.lua
+++ services/debug.lua
@@ -1,11 +1,13 @@
 function CalendarExport.debug(data, name)
+  --@debug@
   name = name or 'CalendarExport Debug Data'
   if ViragDevTool_AddData then
     ViragDevTool_AddData(data, name)
   elseif DevTools_Dump then
-    print(name .. ":")
+    print(name .. ':')
     DevTools_Dump(data)
   else
     print(data)
   end
+  --@end-debug@
 end
